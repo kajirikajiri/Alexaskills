9e0d787876515e6ac364685ff143eedde6bdb022
Kataoka-PHP <kataoka.soma9@gmail.com>
2021-06-06 21:14:15 +0900

    update posts_table

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
     @if (Auth::check())
-        {{ Auth::user()->name }}
+
     @else
         <div class="center jumbotron">
             <div class="text-center">
                 <h1>Alexaskills</h1>
                 {{-- ユーザ登録ページへのリンク --}}
-                {!! link_to_route('signup.get', '新規会員登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
+                {!! link_to_route('signup.get', '', [], ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
         </div>
     @endif
